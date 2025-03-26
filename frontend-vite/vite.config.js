355ebe385e41546deb7411a822cb7d56cc7ae99c
poti <poti@kaoz.org>
2025-03-26 16:27:34 +0100

    fix: minimal

---
+++ frontend-vite/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import flowbiteReact from "flowbite-react/plugin/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), flowbiteReact()],
+})
